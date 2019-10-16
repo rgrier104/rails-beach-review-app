f847f5811d7d6a7c8fc08f4fd8b88e55545dfc2d
rgrier104 <rgrier104@gmail.com>
2019-10-16 12:31:30 -1000

    update review generator with correct belongs_to relationships

--- db/migrate/20191016223048_create_reviews.rb
+++ db/migrate/20191016223048_create_reviews.rb
@@ -1,8 +1,8 @@
 class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
-      t.user :belongs_to
-      t.beach :belongs_to
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :beach, null: false, foreign_key: true
       t.integer :overall_rating
       t.string :overall_description
       t.integer :parking_rating
