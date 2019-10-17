e0eaa84c36ce1a6c09ee5b2506ad76d7f2ae9957
rgrier104 <rgrier104@gmail.com>
2019-10-16 16:04:01 -1000

    add session create logic to login user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,17 @@ class SessionsController < ApplicationController
     end
 
     def new
-        @user = User.new
     end
 
     def create
+        @user = User.find_by(email: params[:user][:email])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.user_id
+            redirect_to user_path(@user)
+        else
+            flash[:message] = "Email and/or password incorrect. Please try again."
+            redirect_to login_path
+        end
     end
 
     def destroy
