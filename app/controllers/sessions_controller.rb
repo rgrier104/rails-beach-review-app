861c05a35cca6a0e8b0c51c417683da5713d7e8b
rgrier104 <rgrier104@gmail.com>
2019-10-16 16:11:58 -1000

    fix session user id field

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     def create
         @user = User.find_by(email: params[:user][:email])
         if @user && @user.authenticate(params[:user][:password])
-            session[:user_id] = @user.user_id
+            session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
             flash[:message] = "Email and/or password incorrect. Please try again."
