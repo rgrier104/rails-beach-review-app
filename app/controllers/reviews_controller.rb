7204b5cf2fe2593c2bfceb8034c1c48b2a28b670
rgrier104 <rgrier104@gmail.com>
2019-10-17 15:13:40 -1000

    add review show view and controller method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,6 +15,10 @@ class ReviewsController < ApplicationController
         end
     end
 
+    def show
+        @review = Review.find_by_id(params[:id])
+    end
+
     private
 
     def review_params
