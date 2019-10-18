05e088c34af62b3de98b8563580bd080e1aced5d
rgrier104 <rgrier104@gmail.com>
2019-10-17 15:10:16 -1000

    add review strong params and create method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,10 +7,17 @@ class ReviewsController < ApplicationController
     end
 
     def create
+        @review = Review.new(review_params)
+        if @review.save
+            redirect_to review_path(@review)
+        else
+            render :new
+        end
     end
 
     private
 
     def review_params
+        params.require(:review).permit(:overall_rating, :overall_description, :parking_rating, :parking_description, :surfing_rating, :surfing_description, :swimming, :snorkeling, :paddleboarding, :sunrise, :sunset, :beach_id, :user_id, beach_attributes: [:name, :city, :shore])
     end
 end
