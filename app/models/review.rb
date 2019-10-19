5589cefcc3acaa9ac0922188a4ac2bf0abc9874b
rgrier104 <rgrier104@gmail.com>
2019-10-18 15:56:49 -1000

    add rating validations for model

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,6 +4,7 @@ class Review < ApplicationRecord
 
   validates :overall_rating, presence: true
   validates :overall_description, presence: true
+  validaes :overall_rating, :parking_rating, :surfing_rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
   validates_associated :beach
 
 
