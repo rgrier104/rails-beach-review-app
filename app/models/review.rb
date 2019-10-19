08b8332c8f46d85d8e036ce0edc631005d030441
rgrier104 <rgrier104@gmail.com>
2019-10-18 15:39:50 -1000

    add validations to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,11 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :beach
+
+  validates :overall_rating, presence: true
+  validates :overall_description, presence: true
+  validates_associated :beach
+
+
   accepts_nested_attributes_for :beach
 end
