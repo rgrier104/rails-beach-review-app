63af608c7467755c28e53332ec06fa65efabb87d
rgrier104 <rgrier104@gmail.com>
2019-10-18 15:49:05 -1000

    ad validations to beach model

--- app/models/beach.rb
+++ app/models/beach.rb
@@ -1,4 +1,8 @@
 class Beach < ApplicationRecord
     has_many :reviews
     has_many :users, through: :reviews
+
+    validates :name, presence: true, uniqueness: true
+    validates :city, presence: true
+    validates :shore, presence: true
 end
