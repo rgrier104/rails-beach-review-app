dc94a5a04579028b1c2da0c14dc0cbe6a4b45a9f
rgrier104 <rgrier104@gmail.com>
2019-10-18 14:07:05 -1000

    add nested route for review in beaches

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,11 @@ Rails.application.routes.draw do
   delete '/logout' => 'sessions#destroy'
 
   resources :reviews
-  resources :beaches
+
+  resources :beaches do 
+    resources :reviews, only: [:index, :new, :create]
+  end
+
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
