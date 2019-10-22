f48692b6e66920cf2422a4b71d8113d1e38fba5a
rgrier104 <rgrier104@gmail.com>
2019-10-22 12:09:51 -1000

    add helper to set or select a beach in review form

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,12 @@
 module ReviewsHelper
+
+    def set_or_select_beach(f)
+        if @beach
+            content_tag(:h2, "Review for #{@beach.name}")
+            f.hidden_field :beach_id
+        else
+            render partial: "beachform", locals: {f: f}
+        end
+    end
+
 end
