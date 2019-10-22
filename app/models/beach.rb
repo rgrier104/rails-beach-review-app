a5dd44a320e1838109555de603e0436cb279d98c
rgrier104 <rgrier104@gmail.com>
2019-10-21 14:26:08 -1000

    add order by rating scope method for beach index

--- app/models/beach.rb
+++ app/models/beach.rb
@@ -5,4 +5,7 @@ class Beach < ApplicationRecord
     validates :name, :city, :shore, presence: true
     validates :name, uniqueness: true
     validates :shore, inclusion: { in: %w{North East South West}, message: "must be either North, South, East, or West."}
+
+    scope :order_by_rating, -> {left_joins(:reviews).group(:id).order("avg(overall_rating) desc")}
+
 end
