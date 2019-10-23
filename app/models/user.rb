924c37d51f5fd75c910301682d28594f91767b49
rgrier104 <rgrier104@gmail.com>
2019-10-23 10:12:25 -1000

    add validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
     has_many :beaches, through: :reviews
     has_secure_password
 
+    validates :firstname, :lastname, :email, presence: true
+    validates :email, uniqueness: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
     def self.from_omniauth(auth)
         where(email: auth.info.email).first_or_initialize do |user|
             user.firstname = auth.info.first_name
