0eb76032ec82b09d2adf2dc5fd3433bbe05328db
rgrier104 <rgrier104@gmail.com>
2019-10-22 16:56:18 -1000

    add omniauth controller method to log user in when using google

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,4 +22,17 @@ class SessionsController < ApplicationController
         session.clear
         redirect_to '/'
     end
+
+    def omniauth
+        @user = User.from_omniauth(auth)
+        @user.save
+        session[:user_id] = @user.id
+        redirect_to user_path(@user.id)
+    end
+
+    private
+
+    def auth
+        request.env['omniauth.auth']
+    end
 end
